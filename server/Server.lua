39d91581e5037b45873b4374c89dcfda31b3bf64
Taxis8 <111230411+Taxis8@users.noreply.github.com>
2023-06-10 20:34:03 +0300

    QBCORE SUPPORT

--- server/Server.lua
+++ server/Server.lua
@@ -1,9 +1,13 @@
-ESX = nil 
-ESX = exports["es_extended"]:getSharedObject()
+if Config.USEESX then
+    ESX = exports["es_extended"]:getSharedObject()
+else
+   QBCore = exports['qb-core']:GetCoreObject()
+end
 discordWebhook = "WEBHOOK" --Change this to your own discord webhook
 
 RegisterNetEvent('taxisCallTaxi:pay')
 AddEventHandler('taxisCallTaxi:pay', function()
+    if Config.USEESX then
     local xPlayer = ESX.GetPlayerFromId(source)
     local price = Config.Price
     local done = false
@@ -18,6 +22,21 @@ AddEventHandler('taxisCallTaxi:pay', function()
         end
     end
     
+    TriggerClientEvent('taxisCallTaxi:isok', source, done)
+    else
+    local player = QBCore.Functions.GetPlayer(source)
+    local price = Config.Price
+    local done = false
+
+    if Player.Functions.RemoveMoney(Config.PriceType, price) then 
+        done = true
+        if Config.DiscordLogs == true then
+            steam = GetPlayerName(source)
+            msg = "Id: " .. source .. " with steam name: " ..steam.. " paied " .. Config.Price.. "$ from his " .. Config.PriceType .. " account for calling a taxi."
+            SendDiscordLog(msg)
+        end
+    end
+
     TriggerClientEvent('taxisCallTaxi:isok', source, done)
 end)
 
