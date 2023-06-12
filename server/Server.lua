757170989c16593b0b2c4282b6bd6156bd73ea3b
Taxis8 <111230411+Taxis8@users.noreply.github.com>
2023-06-12 19:25:54 +0300

    fixed missing end

--- server/Server.lua
+++ server/Server.lua
@@ -8,36 +8,37 @@ discordWebhook = "WEBHOOK" --Change this to your own discord webhook
 RegisterNetEvent('taxisCallTaxi:pay')
 AddEventHandler('taxisCallTaxi:pay', function()
     if Config.USEESX then
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local price = Config.Price
-    local done = false
+        local xPlayer = ESX.GetPlayerFromId(source)
+        local price = Config.Price
+        local done = false
 
-    if xPlayer.getAccount(Config.PriceType).money >= price then
-        xPlayer.removeAccountMoney(Config.PriceType, price)
-        done = true
-        if Config.DiscordLogs == true then
-        steam = GetPlayerName(source)
-        msg = "Id: " .. source .. " with steam name: " ..steam.. " paied " .. Config.Price.. "$ from his " .. Config.PriceType .. " account for calling a taxi."
-        SendDiscordLog(msg)
-        end
-    end
+            if xPlayer.getAccount(Config.PriceType).money >= price then
+                xPlayer.removeAccountMoney(Config.PriceType, price)
+                done = true
+                if Config.DiscordLogs == true then
+                steam = GetPlayerName(source)
+                msg = "Id: " .. source .. " with steam name: " ..steam.. " paied " .. Config.Price.. "$ from his " .. Config.PriceType .. " account for calling a taxi."
+                SendDiscordLog(msg)
+                end
+            end
     
-    TriggerClientEvent('taxisCallTaxi:isok', source, done)
+        TriggerClientEvent('taxisCallTaxi:isok', source, done)
     else
-    local player = QBCore.Functions.GetPlayer(source)
-    local price = Config.Price
-    local done = false
+        local player = QBCore.Functions.GetPlayer(source)
+        local price = Config.Price
+        local done = false
 
-    if Player.Functions.RemoveMoney(Config.PriceType, price) then 
-        done = true
-        if Config.DiscordLogs == true then
-            steam = GetPlayerName(source)
-            msg = "Id: " .. source .. " with steam name: " ..steam.. " paied " .. Config.Price.. "$ from his " .. Config.PriceType .. " account for calling a taxi."
-            SendDiscordLog(msg)
+        if Player.Functions.RemoveMoney(Config.PriceType, price) then 
+            done = true
+            if Config.DiscordLogs == true then
+                steam = GetPlayerName(source)
+                msg = "Id: " .. source .. " with steam name: " ..steam.. " paied " .. Config.Price.. "$ from his " .. Config.PriceType .. " account for calling a taxi."
+                SendDiscordLog(msg)
+            end
         end
-    end
 
-    TriggerClientEvent('taxisCallTaxi:isok', source, done)
+        TriggerClientEvent('taxisCallTaxi:isok', source, done)
+    end
 end)
 
 function SendDiscordLog(message)
